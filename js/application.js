ce7d310580c0f50010f3ac7bce9fdfdb3998493e
Mateusz Grudzien <mateusz.grudzien.pl@gmail.com>
2015-09-07 17:46:57 +0200

    adds navbar with title and reset-and-redraw-grid button

--- js/application.js
+++ js/application.js
@@ -1,9 +1,30 @@
-function drawGrid(width, height, container) {
-	for (var i=0; i<height*width; i++) {
-		$(container).append("<div class='cell'></div>");
+function drawGrid(sketchSideSize) {
+	$('.cell').remove();
+
+	for (var i=0; i<sketchSideSize*sketchSideSize; i++) {
+		$('#cell-container').append("<div class='cell'></div>");
 	}
+
+	$('.cell').on('mouseenter', function() {
+		$(this).addClass('highlighted');
+	});
+}
+
+function resizeCells(sketchSideSize) {
+	var cells = $('.cell');
+	var containerSize = parseInt($('#cell-container').css('height')); //parseInt() ignores non-numeric values
+	var cellSize = containerSize/sketchSideSize;
+	console.log(cellSize);
+	
+	cells.css({"height": + (cellSize-2) + "px", "width": + (cellSize-2) + "px"}); //-2 to account for size of cell's borders 
 }
 
 $(document).ready(function() {
-	drawGrid(70, 50, $('#container'));
+	drawGrid(16);
+
+	$('#reset').on('click', function() {
+		var size = prompt('Enter the size of new sketchpad: ');
+		drawGrid(size);
+		resizeCells(size);
+	});
 });
